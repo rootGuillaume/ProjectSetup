d44dc678e60145e7018bf5a68fb47b4a51eb2a11
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-03 18:16:03 +0200

    Update #3 -- Customed CreateDirectory() function and added CreateFile function

--- project_setup.py
+++ project_setup.py
@@ -1,10 +1,13 @@
 import os
+import webbrowser
 
+projects_dir = "C:\\Users\\gasmi\\PlanetGit\\ProjectSetup\\"
+new_project_dir = input("Pick a directory's name :")
+dir_to_create = projects_dir + new_project_dir
+
+
+# Step 1 -- Create Directories & Files
 def CreateDirectory():
-    projects_dir = "C:\\Users\\gasmi\\PlanetGit\\ProjectSetup\\"
-    new_project_dir = input("Pick a directory's name :")
-    dir_to_create = projects_dir + new_project_dir
-
     try:
         os.mkdir(new_project_dir, 0o755)
         print("Directory '%s' created succesfully" %new_project_dir)
@@ -13,4 +16,18 @@ def CreateDirectory():
     except OSerror as error:
         print("Failed to create directory")
 
+def CreateFile(*files_name):
+    for file in files_name:
+        open(file, "w")
+
 CreateDirectory()
+CreateFile(".gitignore", "README.md")
+
+
+# Step 2 -- Github browser
+
+
+
+#os.chdir(new_project_dir)
+
+#print(os.getcwd())
