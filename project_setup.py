8e08a2e6701dce1e9bb580e46c360bc560c70fd1
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-03 16:06:35 +0200

    Update #2 -- Added CreateDirectory() function

--- project_setup.py
+++ project_setup.py
@@ -1,14 +1,16 @@
 import os
 
-print(os.getcwd())
+def CreateDirectory():
+    projects_dir = "C:\\Users\\gasmi\\PlanetGit\\ProjectSetup\\"
+    new_project_dir = input("Pick a directory's name :")
+    dir_to_create = projects_dir + new_project_dir
 
-projects_dir = "/home/guillaume/Projects/ProjectSetup/"
-new_project_dir = input("Pick a directory's name :")
-dir_to_create = projects_dir + new_project_dir
+    try:
+        os.mkdir(new_project_dir, 0o755)
+        print("Directory '%s' created succesfully" %new_project_dir)
+        print(os.getcwd())
 
-try:
-    os.mkdir(dir_to_create, 0o755)
-    print("Directory '%s' created succesfully" %new_project_dir)
-    print(os.getcwd())
-except OSerror as error:
-    print("Failed to create directory")
+    except OSerror as error:
+        print("Failed to create directory")
+
+CreateDirectory()
