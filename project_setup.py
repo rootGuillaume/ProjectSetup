7251977436f4127c36aa12f46deafb5adf07c65e
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-04 16:23:15 +0200

    Update #4 -- Added githubActions function to navigate into github

--- project_setup.py
+++ project_setup.py
@@ -1,13 +1,14 @@
 import os
-import webbrowser
-
-projects_dir = "C:\\Users\\gasmi\\PlanetGit\\ProjectSetup\\"
-new_project_dir = input("Pick a directory's name :")
-dir_to_create = projects_dir + new_project_dir
+import chromedriver_binary
+from selenium import webdriver
 
 
 # Step 1 -- Create Directories & Files
-def CreateDirectory():
+def createDirectory():
+    projects_dir = "C:\\Users\\gasmi\\PlanetGit\\ProjectSetup\\"
+    new_project_dir = input("Pick a directory's name :")
+    dir_to_create = projects_dir + new_project_dir
+
     try:
         os.mkdir(new_project_dir, 0o755)
         print("Directory '%s' created succesfully" %new_project_dir)
@@ -16,15 +17,37 @@ def CreateDirectory():
     except OSerror as error:
         print("Failed to create directory")
 
-def CreateFile(*files_name):
+
+def createFile(*files_name):
     for file in files_name:
         open(file, "w")
 
-CreateDirectory()
-CreateFile(".gitignore", "README.md")
-
 
 # Step 2 -- Github browser
+
+# WebDriver for Mozilla
+# https://github.com/mozilla/geckodriver/releases
+
+def githubActions():
+    driver = webdriver.Firefox()
+    driver.get("https://github.com/login")
+
+    login_button = driver.find_elements_by_xpath('//*[@id="login_field"]')[0]
+    login_button.send_keys("")
+
+    password_button = driver.find_elements_by_xpath('//*[@id="password"]')[0]
+    password_button.send_keys("")
+
+    sumbit_button = driver.find_elements_by_xpath('//*[@id="login"]/form/div[4]/input[9]')[0]
+    submit_button.click()
+
+#createDirectory()
+#createFile(".gitignore", "README.md")
+githubActions()
+
+
+
+
 
 
 
---
+++ test_setup.py
@@ -0,0 +1,5 @@
+def CreateFile(*files_name):
+    print("Mot :", files_name)
+    #os.open(files_name, "w")
+
+CreateFile("Hello", "World", "Helix")
