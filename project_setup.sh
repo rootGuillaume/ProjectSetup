9e85aed64f992a35f0ef10277441991a0fbda697
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-07 16:46:37 +0200

    Update #16 -- Change project_setup.sh name

--- project_setup.sh
+++ project_setup.sh
@@ -1,5 +1,5 @@
 function new_project() {
-  python3 project_setup.py $1
+  #python3 project_setup.py $1
   cd /Users/user/PlanetGit/$1
   git init
   git remote add origin git@github.com:user/$1.git
