4b7ee7363ea84e8b67db17b728d30b32e5ce1508
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-08 20:25:45 +0200

    Update #23 -- Setted up github log in with username & password

--- project.py
+++ project.py
@@ -7,7 +7,14 @@ def new_project():
     project = str(sys.argv[1])
     os.mkdir(project)
     browser = webdriver.Firefox()
-    browser.get("https://github.com/login")
+    browser.get('https://github.com/login')
+    login_input = browser.find_element_by_id('login_field')
+    login_input.send_keys("login")
+    password_input = browser.find_element_by_id('password')
+    password_input.send_keys("password")
+    login_submit = browser.find_element_by_name('commit')
+    login_submit.submit()
+
 
 if __name__ == '__main__':
     new_project()
