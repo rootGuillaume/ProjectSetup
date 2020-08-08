1d21abe94b5e47d31c718766a32c86a700bf81cb
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-08 21:06:04 +0200

    Update #24 -- Setted up new repository creation

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 from selenium import webdriver
 
 
@@ -8,13 +9,21 @@ def new_project():
     os.mkdir(project)
     browser = webdriver.Firefox()
     browser.get('https://github.com/login')
+
     login_input = browser.find_element_by_id('login_field')
-    login_input.send_keys("login")
+    login_input.send_keys("")
     password_input = browser.find_element_by_id('password')
-    password_input.send_keys("password")
+    password_input.send_keys("")
     login_submit = browser.find_element_by_name('commit')
     login_submit.submit()
 
+    time.sleep(1.5)
+
+    browser.get('https://github.com/new')
+    repo_name = browser.find_element_by_id('repository_name')
+    repo_name.send_keys(project)
+    repo_submit = browser.find_element_by_id('new_repository')
+    repo_submit.submit()
 
 if __name__ == '__main__':
     new_project()
