fb49e932432dbd03a867638b11908696ab55cb47
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-08 20:17:33 +0200

    Update #22 -- Import firefox webdriver & open github login page

--- project.py
+++ project.py
@@ -1,12 +1,13 @@
 import os
 import sys
-import selenium
+from selenium import webdriver
 
 
 def new_project():
     project = str(sys.argv[1])
     os.mkdir(project)
-    
+    browser = webdriver.Firefox()
+    browser.get("https://github.com/login")
 
 if __name__ == '__main__':
     new_project()
