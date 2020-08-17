969e274e203625e2b7276e37e3478e9b816be25a
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-17 12:41:50 +0200

    Update #80 -- Added web project structure into webproject.sh script

--- WebProject/webproject.sh
+++ WebProject/webproject.sh
@@ -1,16 +1,27 @@
 function webproject() {
   echo "### Running script... Please wait ###"
+
   cd
   mkdir $1
   project.py $1
   cd ./$1
+
   git init
   git remote add origin https://github.com/rootGuillaume/$1.git
+
+  mkdir {css,fonts,img,js,components}
+
+
   touch README.md
   touch .gitignore
+  touch index.html
+  touch /css/styles.css
+  touch /js/script.js 
+
   git add .
   git commit -m "first commit"
   git push origin master
   #atom
+
   echo "### Process complete ###"
 }
