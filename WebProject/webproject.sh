4c606da096ecc13deddaa2aa24790cb2cae67ce7
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-17 12:27:28 +0200

    Update #78 -- Copied project.sh content into webproject.sh

---
+++ WebProject/webproject.sh
@@ -0,0 +1,16 @@
+function project() {
+  echo "### Running script... Please wait ###"
+  cd
+  mkdir $1
+  project.py $1
+  cd ./$1
+  git init
+  git remote add origin https://github.com/rootGuillaume/$1.git
+  touch README.md
+  touch .gitignore
+  git add .
+  git commit -m "first commit"
+  git push origin master
+  #atom
+  echo "### Process complete ###"
+}
